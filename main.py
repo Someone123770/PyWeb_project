f35adcaf491b89659eeabbbdde3bcd83dc7ddda5
Michael <magmagmag2104@gmail.com>
2023-04-20 13:59:58 +0300

    finished sign in and registration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from forms.loginform import LoginForm
+from forms.registerform import RegisterForm
 
 app = Flask(__name__)
 
@@ -9,9 +10,9 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/index')
 def index():
     print('start_index')
-    flag = int(input("Зарегистрирован ли пользователь?"))
+    # flag = int(input("Зарегистрирован ли пользователь?"))
     # url_to_img = input("Введите путь к аватарке")
-    return render_template('base.html', flag=flag)
+    return render_template('index.html', flag=0)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,6 +20,13 @@ def login():
     if form.validate_on_submit():
         return redirect('/')
     return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/registration', methods=['GET', 'POST'])
+def reg():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('registration.html', title='регистрация', form=form)
 
 
 if __name__ == '__main__':
