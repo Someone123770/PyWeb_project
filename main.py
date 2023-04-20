ec4cbf2904b209f492ac42af08bd82d254461e86
Michael <magmagmag2104@gmail.com>
2023-04-20 03:53:46 +0300

    almost finished sign in

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
 @app.route('/index')
@@ -9,6 +12,13 @@ def index():
     flag = int(input("Зарегистрирован ли пользователь?"))
     # url_to_img = input("Введите путь к аватарке")
     return render_template('base.html', flag=flag)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
